f126689894feffc7ac10265dae434658e92fa850
deborahsag <85948728+deborahsag@users.noreply.github.com>
2023-06-29 09:30:50 -0300

    Added MIREX unit tests.

---
+++ test_mirex.py
@@ -0,0 +1,22 @@
+import unittest
+
+# from mirex import *
+
+
+class TestMIREX(unittest.TestCase):
+
+    def test_get_prompt(self):
+        res = 0
+        assert res == 0
+
+    def test_get_continuations(self):
+        res = 0
+        assert res == 0
+
+    def test_compute_continuation_prob(self):
+        res = 0
+        assert res == 0
+
+
+if __name__ == '__main__':
+    unittest.main()
