6a46aed58b1672c8588da8e21071b9959b4c386c
deborahsag <85948728+deborahsag@users.noreply.github.com>
2023-11-09 14:11:54 -0300

    Data augmentation and preprocess work and the code prevents rework.

--- preprocess_and_augment_midi.py
+++ preprocess_and_augment_midi.py
@@ -1,3 +1,15 @@
+"""
+Observacoes
+
+Pode tambem verificar no preprocess se a musica ja foi copiada pra evitar copiar de novo
+
+test.json
+train: 8
+test: 4
+validation: 7
+
+"""
+
 import argparse
 import json
 import os
@@ -9,12 +21,12 @@ import pretty_midi
 from third_party.midi_processor.processor import encode_midi
 
 pitch_shift = [-3, -2, -1, 0, 1, 2, 3]
-tempo_shift = [0.95, 0.975, 1, 1.025, 1.05]
+time_stretch = [0.95, 0.975, 1, 1.025, 1.05]
 
-JSON_FILE = "maestro-v2.0.0.json"
+JSON_FILE = "test.json"
 
 
-def augment_midi(midi, pitch, tempo):
+def augment_midi(midi_path, pitch, tempo):
     """
     ----------
     Authors:  Gabriel Souza, Deborah Guimarães
@@ -23,17 +35,17 @@ def augment_midi(midi, pitch, tempo):
     ----------
     """
 
-    print(f"Doing for {midi}")
-    dir_split, extension_split = midi.rfind("/") + 1, midi.rfind(".midi") if ".midi" in midi else midi.rfind(".MIDI")
-    filepath, filename, extension = midi[:dir_split], midi[dir_split:extension_split], midi[extension_split:]
+    print(f"Doing for {midi_path}")
+    dir_split, extension_split = midi_path.rfind("/") + 1, midi_path.rfind(".midi") if ".midi" in midi_path else midi_path.rfind(".MIDI")
+    filepath, filename, extension = midi_path[:dir_split], midi_path[dir_split:extension_split], midi_path[extension_split:]
 
-    mid = pretty_midi.PrettyMIDI(midi)
+    midi = pretty_midi.PrettyMIDI(midi_path)
 
-    mid.remove_invalid_notes()
-    mid.instruments = [instr for instr in mid.instruments if not instr.is_drum]     # remove percussion
+    midi.remove_invalid_notes()
+    midi.instruments = [instr for instr in midi.instruments if not instr.is_drum]     # remove percussion
 
-    new_mid = deepcopy(mid)
-    for instr in new_mid.instruments:
+    new_midi = deepcopy(midi)
+    for instr in new_midi.instruments:
         for note in instr.notes:
             note.pitch += pitch
             if note.pitch > 127:
@@ -43,16 +55,15 @@ def augment_midi(midi, pitch, tempo):
             note.start, note.end = note.start * tempo, note.end * tempo
 
     print(f"Writing {filepath}Aug-{filename}-{pitch}-{tempo}{extension}")
-    new_mid.write(f"{filepath}Aug-{filename}-{pitch}-{tempo}{extension}")
+    new_midi.write(f"{filepath}Aug-{filename}-{pitch}-{tempo}{extension}")
 
     return True
-
 
 
 def prep_maestro_midi_aug(maestro_root, output_dir):
     """
     ----------
-    Author: Damon Gwinn, alterations: Gabriel Souza, Deborah Guimarães
+    Author: Damon Gwinn, Deborah Guimarães, Gabriel Souza
     ----------
     Pre-processes the maestro dataset, putting processed midi data (train, eval, test) into the
     given output folder, while making the data augmentation for the train data
@@ -79,15 +90,17 @@ def prep_maestro_midi_aug(maestro_root, output_dir):
     train_count = 0
     val_count   = 0
     test_count  = 0
+    aug_count   = 0
+    skip_count  = 0
 
     for piece in maestro_json:
-        mid         = os.path.join(maestro_root, piece["midi_filename"])
+        midi_path         = os.path.join(maestro_root, piece["midi_filename"])
         split_type  = piece["split"]
-        f_name      = mid.split("/")[-1] + ".pickle"
+        f_name      = midi_path.split("/")[-1] + ".pickle"
 
         if split_type == "train":
             o_file = os.path.join(train_dir, f_name)
-            train_count += 1
+            # train_count += 1      # comment this line to prevent counting non-augmented data twice
         elif split_type == "validation":
             o_file = os.path.join(val_dir, f_name)
             val_count += 1
@@ -98,29 +111,45 @@ def prep_maestro_midi_aug(maestro_root, output_dir):
             print("ERROR: Unrecognized split type:", split_type)
             return False
 
-        prepped = encode_midi(mid)
+        if os.path.exists(o_file):
+            skip_count += 1
+        else:
+            prepped = encode_midi(midi_path)
+            o_stream = open(o_file, "wb")
+            pickle.dump(prepped, o_stream)
+            o_stream.close()
 
-        o_stream = open(o_file, "wb")
-        pickle.dump(prepped, o_stream)
-        o_stream.close()
-        
         if split_type == "train":
             for pitch in pitch_shift:
-                for tempo in tempo_shift:
-                    if (pitch == 0 and tempo == 1) or os.path.exists(f"{output_dir}/train/Aug-{piece['midi_filename']}-{pitch}-{tempo}.pickle"):
+                for tempo in time_stretch:
+                    train_count += 1
+
+                    dir_split, extension_split = midi_path.rfind("/") + 1, midi_path.rfind(".midi") if ".midi" in midi_path else midi_path.rfind(".MIDI")
+                    filepath, filename, extension = midi_path[:dir_split], midi_path[dir_split:extension_split], midi_path[extension_split:]
+                    midi_aug_path = f"{filepath}Aug-{filename}-{pitch}-{tempo}{extension}"
+
+                    # if (pitch == 0 and tempo == 1) and not os.path.exists(midi_aug_path):
+                    #     augment_midi(midi_path, pitch, tempo)
+                    #
+                    # if pitch == 0 and tempo == 1:
+                    #     continue
+
+                    if pitch == 0 and tempo == 1:
                         continue
+                    if not os.path.exists(midi_aug_path):
+                        augment_midi(midi_path, pitch, tempo)
+                        aug_count += 1
 
-                    augment_midi(mid, pitch, tempo)
-
-                    piece_name = piece["midi_filename"]
-                    name_aug = f"Aug-{piece_name}-{pitch}-{tempo}.midi"
-                    mid_aug    = os.path.join(maestro_root, name_aug)
-                    f_name_aug = mid_aug.split("/")[-1] + ".pickle"
+                    f_name_aug = midi_aug_path.split("/")[-1] + ".pickle"
                     o_file_aug = os.path.join(train_dir, f_name_aug)
-                    prepped_aug = encode_midi(mid_aug)
-                    o_stream_aug = open(o_file_aug, "wb")
-                    pickle.dump(prepped_aug, o_stream_aug)
-                    o_stream_aug.close()
+
+                    if os.path.exists(o_file_aug):
+                        skip_count += 1
+                    else:
+                        prepped_aug = encode_midi(midi_aug_path)
+                        o_stream_aug = open(o_file_aug, "wb")
+                        pickle.dump(prepped_aug, o_stream_aug)
+                        o_stream_aug.close()
 
         total_count += 1
         if total_count % 50 == 0:
@@ -129,6 +158,8 @@ def prep_maestro_midi_aug(maestro_root, output_dir):
     print("Num Train:", train_count)
     print("Num Val:", val_count)
     print("Num Test:", test_count)
+    print("Num Augmented:", aug_count)
+    print("Num Skipped:", skip_count)
     return True
 
 
