ef14ad6a1957c829062adda3c3f210595c5debf3
deborahsag <deborahsantosag@gmail.com>
2023-12-19 18:32:02 -0300

    Added preprocess and augment for custom data.

--- preprocess_and_augment_midi.py
+++ preprocess_and_augment_midi.py
@@ -4,6 +4,8 @@ import os
 import pickle
 from copy import deepcopy
 
+import random
+
 import pretty_midi
 
 from third_party.midi_processor.processor import encode_midi
@@ -11,7 +13,10 @@ from third_party.midi_processor.processor import encode_midi
 pitch_shift = [-3, -2, -1, 0, 1, 2, 3]
 time_stretch = [0.95, 0.975, 1, 1.025, 1.05]
 
-JSON_FILE = "test.json"
+JSON_FILE = "maestro-v2.0.0.json"
+
+SEED = 4242
+random.seed(SEED)
 
 
 def augment_midi(midi_path, pitch, tempo):
@@ -24,8 +29,8 @@ def augment_midi(midi_path, pitch, tempo):
     """
 
     print(f"Doing for {midi_path}")
-    dir_split, extension_split = midi_path.rfind("/") + 1, midi_path.rfind(".midi") if ".midi" in midi_path else midi_path.rfind(".MIDI")
-    filepath, filename, extension = midi_path[:dir_split], midi_path[dir_split:extension_split], midi_path[extension_split:]
+    filepath, file_name_with_extension = os.path.split(midi_path)
+    filename, extension = os.path.splitext(file_name_with_extension)
 
     midi = pretty_midi.PrettyMIDI(midi_path)
 
@@ -42,8 +47,8 @@ def augment_midi(midi_path, pitch, tempo):
                 note.pitch += 12
             note.start, note.end = note.start * tempo, note.end * tempo
 
-    print(f"Writing {filepath}Aug-{filename}-{pitch}-{tempo}{extension}")
-    new_midi.write(f"{filepath}Aug-{filename}-{pitch}-{tempo}{extension}")
+    print(f"Writing {filepath}/Aug-{filename}-{pitch}-{tempo}{extension}")
+    new_midi.write(f"{filepath}/Aug-{filename}-{pitch}-{tempo}{extension}")
 
     return True
 
@@ -82,7 +87,7 @@ def prep_maestro_midi_aug(maestro_root, output_dir):
     skip_count  = 0
 
     for piece in maestro_json:
-        midi_path         = os.path.join(maestro_root, piece["midi_filename"])
+        midi_path   = os.path.join(maestro_root, piece["midi_filename"])
         split_type  = piece["split"]
         f_name      = midi_path.split("/")[-1] + ".pickle"
 
@@ -112,9 +117,9 @@ def prep_maestro_midi_aug(maestro_root, output_dir):
                 for tempo in time_stretch:
                     train_count += 1
 
-                    dir_split, extension_split = midi_path.rfind("/") + 1, midi_path.rfind(".midi") if ".midi" in midi_path else midi_path.rfind(".MIDI")
-                    filepath, filename, extension = midi_path[:dir_split], midi_path[dir_split:extension_split], midi_path[extension_split:]
-                    midi_aug_path = f"{filepath}Aug-{filename}-{pitch}-{tempo}{extension}"
+                    filepath, file_name_with_extension = os.path.split(midi_path)
+                    filename, extension = os.path.splitext(file_name_with_extension)
+                    midi_aug_path = f"{filepath}/Aug-{filename}-{pitch}-{tempo}{extension}"
 
                     if pitch == 0 and tempo == 1:
                         continue
@@ -145,6 +150,103 @@ def prep_maestro_midi_aug(maestro_root, output_dir):
     return True
 
 
+def prep_custom_midi_aug(custom_midi_root, output_dir, valid_p=0.1, test_p=0.2):
+    """
+    ----------
+    Author: Corentin Nelias, Deborah Guimarães
+    ----------
+    Pre-processes custom midi files that are not part of the maestro dataset, putting processed midi data (train, eval,
+    test) into the given output folder while augmenting the training data.
+    ----------
+    """
+    train_dir = os.path.join(output_dir, "train")
+    os.makedirs(train_dir, exist_ok=True)
+    val_dir = os.path.join(output_dir, "val")
+    os.makedirs(val_dir, exist_ok=True)
+    test_dir = os.path.join(output_dir, "test")
+    os.makedirs(test_dir, exist_ok=True)
+
+    print("Found", len(os.listdir(custom_midi_root)), "pieces")
+    print("Preprocessing custom data...")
+    total_count = 0
+    train_count = 0
+    val_count   = 0
+    test_count  = 0
+    aug_count   = 0
+    skip_count  = 0
+
+    for piece in os.listdir(custom_midi_root):
+        # deciding whether the data should be part of train, valid or test dataset
+        is_train = True if random.random() > valid_p else False
+        if not is_train:
+            is_valid = True if random.random() > test_p else False
+        if is_train:
+            split_type = "train"
+        elif is_valid:
+            split_type = "validation"
+        else:
+            split_type = "test"
+
+        midi_path   = os.path.join(custom_midi_root, piece)
+        f_name      = piece.split(".")[0] + ".pickle"
+
+        if split_type == "train":
+            o_file = os.path.join(train_dir, f_name)
+            # train_count += 1      # comment this line to prevent counting non-augmented data twice
+        elif split_type == "validation":
+            o_file = os.path.join(val_dir, f_name)
+            val_count += 1
+        elif split_type == "test":
+            o_file = os.path.join(test_dir, f_name)
+            test_count += 1
+
+        if os.path.exists(o_file):
+            skip_count += 1
+        else:
+            prepped = encode_midi(midi_path)
+            o_stream = open(o_file, "wb")
+            pickle.dump(prepped, o_stream)
+            o_stream.close()
+
+        if split_type == "train":
+            for pitch in pitch_shift:
+                for tempo in time_stretch:
+                    train_count += 1
+
+                    filepath, file_name_with_extension = os.path.split(midi_path)
+                    filename, extension = os.path.splitext(file_name_with_extension)
+
+                    midi_aug_path = f"{filepath}/Aug-{filename}-{pitch}-{tempo}{extension}"
+
+                    if pitch == 0 and tempo == 1:
+                        continue
+                    if not os.path.exists(midi_aug_path):
+                        augment_midi(midi_path, pitch, tempo)
+                        aug_count += 1
+
+                    f_name_aug = midi_aug_path.split("/")[-1] + ".pickle"
+                    o_file_aug = os.path.join(train_dir, f_name_aug)
+
+                    if os.path.exists(o_file_aug):
+                        skip_count += 1
+                    else:
+                        prepped_aug = encode_midi(midi_aug_path)
+                        o_stream_aug = open(o_file_aug, "wb")
+                        pickle.dump(prepped_aug, o_stream_aug)
+                        o_stream_aug.close()
+
+        total_count += 1
+        if total_count % 50 == 0:
+            print(total_count, "/", len(os.listdir(custom_midi_root)))
+
+    print("Num Train:", train_count)
+    print("Num Val:", val_count)
+    print("Num Test:", test_count)
+    print("Num Augmented:", aug_count)
+    print("Num Skipped:", skip_count)
+    return True
+
+
 def parse_args():
     """
     ----------
@@ -157,8 +259,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("root", type=str, help="Root folder for the Maestro dataset or for custom data.")
-    parser.add_argument("-output_dir", type=str, default="./dataset/e_piano", help="Output folder to put the preprocessed midi into.")
-    # parser.add_argument("--custom_dataset", action="store_true", help="Whether or not the specified root folder contains custom data.")
+    parser.add_argument("-output_dir", type=str, default="./dataset/maestro", help="Output folder to put the preprocessed midi into.")
+    parser.add_argument("--custom_dataset", action="store_true", help="Whether or not the specified root folder contains custom data.")
 
     return parser.parse_args()
 
@@ -172,12 +274,16 @@ def main():
     ----------
     """
 
-    args            = parse_args()
-    root            = args.root
-    output_dir      = args.output_dir
+    args                = parse_args()
+    root                = args.root
+    output_dir          = args.output_dir
+    is_custom_dataset   = args.custom_dataset
 
     print("Preprocessing midi files and saving to", output_dir)
-    prep_maestro_midi_aug(root, output_dir)
+    if is_custom_dataset:
+        prep_custom_midi_aug(root, output_dir)
+    else:
+        prep_maestro_midi_aug(root, output_dir)
     print("Done!")
     print("")
 
