c4b9e2d4f14ec1ffce6c37b4cb23edc58c00e4ff
deborahsag <deborahsantosag@gmail.com>
2024-05-02 15:22:28 -0300

    Correcting miditiok token constants.

---
+++ utilities/constants_miditok.py
@@ -0,0 +1,30 @@
+import torch
+
+
+SEPERATOR               = "========================="
+
+# Taken from the paper
+ADAM_BETA_1             = 0.9
+ADAM_BETA_2             = 0.98
+ADAM_EPSILON            = 10e-9
+
+LR_DEFAULT_START        = 1.0
+SCHEDULER_WARMUP_STEPS  = 4000
+# LABEL_SMOOTHING_E       = 0.1
+
+# DROPOUT_P               = 0.1
+
+TOKEN_END               = 2
+TOKEN_PAD               = 0
+
+# REMI
+# VOCAB_SIZE              = 530
+# TSD
+VOCAB_SIZE              = 497
+
+TORCH_FLOAT             = torch.float32
+TORCH_INT               = torch.int32
+
+TORCH_LABEL_TYPE        = torch.long
+
+PREPEND_ZEROS_WIDTH     = 4
